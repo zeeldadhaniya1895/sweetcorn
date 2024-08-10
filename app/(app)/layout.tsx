f8f560765fa475a4f195c84a9ff956e4cbb3ea46
zeeldadhaniya1895 <zeeldadhaniya1895@gmail.com>
2024-08-10 17:50:56 +0530

    "auth complete"

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import Navbar from "@/app/components/Navbar";
 import "@/app/globals.css"
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <SessionProvider>
       <Navbar/>
+      </SessionProvider >
         {children}
       </body>
     </html>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,23 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import UserButton from "@/components/auth/user-button";
+
+
 
 export default function Navbar() {
+  const { data: session, status } = useSession();
+
   return (
+
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-[#043A3A] flex items-center">
             <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
                 <div className="relative left-2 hidden md:block">
                     <span className="text-white font-bold ">Sweet</span><span className="text-[#2FB95D] font-bold">Corn</span>
                 </div>
-                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+
+                {!(status==="authenticated") ? <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
                     <Button size="sm" variant="outline" asChild >
                         <Link href="/auth/login">
                             Login
@@ -21,7 +30,9 @@ export default function Navbar() {
                         </Link>
 
                     </Button>
-                </div>
+                </div>: <UserButton/>}
+
+                
             </div>
         </div>
   )
